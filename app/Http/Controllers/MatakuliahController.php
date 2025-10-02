555a8b62df79e9d7c470eff717fbeb541cea349a
Nailah24-TI <nailah24ti@mahasiswa.pcr.ac.id>
2025-10-02 08:13:12 +0700

    ada perubahan

---
+++ app/Http/Controllers/MatakuliahController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MatakuliahController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return ('Menampilkan data Matkuliah');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $param1 = '')
+    {
+        // if($param1 == 'ST445'){
+        //     return ('Anda mengakses Matakuliah ST445');
+        if($param1){
+            return ('Anda mengakses Matakuliah'.$param1);
+        }else{
+            return ('Masukkan kode Matakuliah !');
+        }    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/halaman-about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content
+    ="width=device-width, initial-scale=1.0">
+    <title>About Page</title>
+</head>
+<body>
+    <h1>Ini adalah halaman About</h1>
+    <p>Laravel membuat development lebih cepat</p>
+</body>
+</html>
